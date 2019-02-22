61b039d457aee3ef2cca705fd85f2e4cf06ace43
amreen.noor <amreen.noor@thomsonreuters.com>
2019-02-22 12:27:22 +0530

    Add

--- ServiceNowQAAutomation/SNOWQA/src/test/java/testcase/ChangeReg.java
+++ ServiceNowQAAutomation/SNOWQA/src/test/java/testcase/ChangeReg.java
@@ -1,11 +1,18 @@
 package testcase;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -18,6 +25,7 @@ import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.ChartLocation;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.relevantcodes.extentreports.LogStatus;
 import com.servicenow.applicationspecificlibraries.ChangeReusables;
 import com.servicenow.applicationspecificlibraries.ProblemReusables;
 import com.servicenow.applicationspecificlibraries.SafeLogin;
@@ -25,9 +33,13 @@ import com.servicenow.applicationspecificlibraries.ServiceNowUtils;
 import com.servicenow.applicationspecificlibraries.WaitUtils;
 import com.servicenow.genericlibraries.Capabilities;
 import com.servicenow.genericlibraries.DropDowns;
+import com.servicenow.genericlibraries.ExcelUtils;
 import com.servicenow.genericlibraries.ExtentReport;
+import com.servicenow.genericlibraries.ReporterLogs;
+import com.servicenow.genericlibraries.TextBoxes;
 
 import pages.ChangePage;
+import pages.ProblemPage;
 
 public class ChangeReg {
 	ExtentHtmlReporter htmlReporter;
@@ -35,6 +47,7 @@ public class ChangeReg {
 	ExtentTest logger;
 	static String crNumber=null;
 	static String prNumber=null;
+	static String cTask=null;
 	@BeforeTest
 	public void startReport(){
 		
@@ -52,13 +65,11 @@ public class ChangeReg {
 	
 	@Test(priority=0,description="Creation of a CR Ticket",enabled=true)
 	public void testCreateChnageRequest() throws IOException, InterruptedException{
-		
 		logger = extent.createTest("Creation of a CR Ticket");
 		String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
 		System.setProperty("webdriver.gecko.driver",FirefoxDriver);
 		WebDriver driver=new FirefoxDriver();
-		
-	    driver.get("http://thomsonreutersqa.service-now.com");
+		driver.get("http://thomsonreutersqa.service-now.com");
 	    SafeLogin.logInUser(driver);
 		WaitUtils.waitForPageToLoad(driver, 10);
 		ServiceNowUtils.navigateToModuleName2(driver, "change");
@@ -67,7 +78,6 @@ public class ChangeReg {
 	    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
 		WaitUtils.waitForPageToLoad(driver, 10);
 		ChangeReusables.verifyStateOfChangeTicket(driver, "Draft", crNumber,1,2);
-		//ChangeReusables.FinalReport(driver, "Draft", crNumber, 1, 2);
 		driver.close();
 		Assert.assertTrue(true);
 		logger.log(Status.PASS, MarkupHelper.createLabel("Test Case Passed is Creation of a CR Ticket", ExtentColor.GREEN));
@@ -225,7 +235,7 @@ public void testCreateNormalChangeRequestN() throws IOException, InterruptedExce
 
 
 @Test(priority=2,description="Approval of chnage ticket and Moving the Change to Multiple phase",enabled=true)
-public void testMovingtoMultiplePhases() throws Exception{
+public void testMovingtoMultiplePhasesEmer() throws Exception{
 	logger = extent.createTest("Creation of a CR Ticket");
 	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
 	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
@@ -309,11 +319,320 @@ public void testCreateNormalChnageRequestN() throws IOException, InterruptedExce
 			System.out.println("invisible");
 		
 		}
+	
 
 }
 
+@Test(priority=0,description="Creation of a CR Ticket",enabled=true)
+public void testCreateNormalChnageRequestfromproblem() throws Exception{
+	
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+    ServiceNowUtils.navigateToModuleName2(driver, "Problem");
+    prNumber=ProblemReusables.createProblem(driver,1,2);
+	   ProblemReusables.verifyProblemCreation1(driver, prNumber); 
+	   ProblemPage.getProblemNumberFromQueue(driver, prNumber).click();
+	   WaitUtils.waitForPageToLoad(driver, 30);
+	   
+	   ProblemPage.getMenu(driver).click();
+	   Thread.sleep(3000);
+	   //ProblemPage.getChangeLink(driver).click();
+	    WebElement element=driver.findElement(By.xpath("/html/body/div[8]/div[5]"));
+	    new Actions(driver).moveToElement(element).perform();
+
+	   
+	    
+	 // ProblemPage.getChangeLink(driver).click();
+    /*WaitUtils.waitForPageToLoad(driver, 30);
+    String source = ExcelUtils.getData("Problem_Management_TestData.xlsx","Smoke_Suite", 1, 5);
+    String assignmentGroup = ExcelUtils.getData("Problem_Management_TestData.xlsx","Smoke_Suite", 1, 6);
+    String configurationItem = ExcelUtils.getData("Problem_Management_TestData.xlsx","Smoke_Suite", 1, 7);
+    String shortDescription = ExcelUtils.getData("Problem_Management_TestData.xlsx","Smoke_Suite", 1, 10);
+    String description = ExcelUtils.getData("Problem_Management_TestData.xlsx","Smoke_Suite", 1, 11);
+    WaitUtils.waitForIdPresent(driver, "sys_readonly.problem.number");
+    String problemId = driver.findElement(By.xpath("//input[@id='sys_readonly.problem.number']")).getAttribute("value");
+    System.out.println(problemId);
+    int sNum = 0;
+   
+	int cellNum = 0;
+	ExcelUtils.writeDataIntoCell("Problem_Management_TestData.xlsx","Smoke_Suite", 1, 2, problemId);
+   // ExtentReport.reportLog(LogStatus.INFO, "Problem number captured: "+problemId);
+   // ReporterLogs.log("Problem number captured: "+problemId, "info");     
+    WaitUtils.waitForIdPresent(driver, "problem.contact_type");
+    DropDowns.selectDropdownByVisibleText(ProblemPage.getSourceDropdown(driver), source, "Source");
+    //Thread.sleep(3000);                                 
+    WaitUtils.waitForIdPresent(driver, "sys_display.problem.assignment_group");
+    TextBoxes.enterTextValue(ProblemPage.getAssignmentGrpEdt(driver), assignmentGroup, "Assignment Group");                    
+    ProblemPage.getAssignmentGrpEdt(driver).sendKeys(Keys.ENTER);
+    ProblemPage.getAssignmentGrpEdt(driver).sendKeys(Keys.DOWN);
+    ProblemPage.getAssignmentGrpEdt(driver).sendKeys(Keys.ENTER);
+    Thread.sleep(5000);
+    ReporterLogs.log("Entering Assignment group field value "+assignmentGroup, "info");                 
+    TextBoxes.enterTextValue(ProblemPage.getShortdescriptionEdt(driver), shortDescription, "Short Description");
+    Thread.sleep(2000);
+    TextBoxes.enterTextValue(ProblemPage.getDescriptionEdt(driver), description, "Description");
+    Thread.sleep(2000);
+    WaitUtils.waitForIdPresent(driver, "sys_display.problem.cmdb_ci"); 
+    TextBoxes.enterTextValue(ProblemPage.getConfigurationitemEdt(driver), configurationItem, "Configuration Item");
+    ProblemPage.getConfigurationitemEdt(driver).sendKeys(Keys.ENTER);
+    ReporterLogs.log("Entering Configuration Item field value "+configurationItem, "info");
+    Thread.sleep(5000);
+    ProblemPage.getSubmitBtn(driver).click();*/
 
 
+}
+
+@Test(priority=0,description="Creation of a CR Ticket",enabled=true)
+public void testButtonVerification() throws IOException, InterruptedException{
+	
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	
+    driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ServiceNowUtils.navigateToModuleName2(driver, "change");
+	crNumber = ChangeReusables.createChange(driver,1,2);
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
+	WaitUtils.waitForPageToLoad(driver, 10);
+	if( driver.findElement(By.xpath("//button[text()='Update']")).isDisplayed()&&
+			driver.findElement(By.xpath("//button[text()='Copy Change']")).isDisplayed()&&
+			driver.findElement(By.xpath("//button[text()='Submit for Planning']")).isDisplayed()){
+		driver.close();
+		Assert.assertTrue(true);
+		logger.log(Status.PASS, MarkupHelper.createLabel("Test Case passed", ExtentColor.GREEN));
+		}else{
+			System.out.println("invisible");
+		
+		}
+	
+}
+
+@Test(priority=0,description="Creation of a CR Ticket",enabled=true)
+public void testDefaulttaskvalidation() throws IOException, InterruptedException{
+	
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	
+    driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ServiceNowUtils.navigateToModuleName2(driver, "change");
+	crNumber = ChangeReusables.createChange(driver,1,2);
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ChangePage.getSubmitForPlanningBtn(driver).click();
+	Thread.sleep(10000);
+	ChangePage.getChangeTaskTab(driver).click();
+	System.out.println("success");
+	ChangePage.getChangeTaskLnk(driver, 1).click();
+	cTask=ChangePage.getTaskChangeEdt(driver).getAttribute("value");
+	if(cTask!=null)
+	{
+		System.out.println(cTask);
+		driver.close();
+		Assert.assertTrue(true);
+		logger.log(Status.PASS, MarkupHelper.createLabel("Test Case passed", ExtentColor.GREEN));
+	}
+	else
+	{
+		System.out.println("fail");
+	}
+	
+}
+@Test(priority=2,description="Approval of chnage ticket and Moving the Change to Multiple phase",enabled=true)
+public void testStartAndEnd() throws Exception{
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	
+    driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ServiceNowUtils.navigateToModuleName2(driver, "change");
+	crNumber = ChangeReusables.createEmerChange(driver,1,2);
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ChangeReusables.verifyStateOfChangeTicket(driver, "Draft", crNumber,1,2);
+	ChangePage.getSubmitForPlanningBtn(driver).click();
+	Thread.sleep(10000);
+    ChangeReusables.moveToAssessmentState(driver);
+    ChangePage.getSaveBtn(driver).click();
+    if( driver.findElement(By.xpath("/html/body/div[2]/form/span[1]/span/div[2]/div/div/span[2]")).isDisplayed()){
+		driver.close();
+		Assert.assertTrue(true);
+		logger.log(Status.PASS, MarkupHelper.createLabel("Test Case passed", ExtentColor.GREEN));
+		}else{
+			System.out.println("invisible");
+		
+		}
+}
+@Test(priority=2,description="Approval of chnage ticket and Moving the Change to Multiple phase",enabled=true)
+public void testFollowUp() throws Exception{
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	
+    driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ServiceNowUtils.navigateToModuleName2(driver, "change");
+	crNumber = ChangeReusables.createEmerChange(driver,1,2);
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ChangeReusables.verifyStateOfChangeTicket(driver, "Draft", crNumber,1,2);
+	ChangePage.getSubmitForPlanningBtn(driver).click();
+	Thread.sleep(10000);
+    ChangeReusables.moveToAssessmentState(driver);
+	ChangeReusables.FinalReport(driver, "Assessment", crNumber, 2, 2);
+    ChangeReusables.schedule(driver);
+	ChangePage.getUpdateBtn(driver).click();
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+	ChangePage.getChangeNumberFromQueue(driver, crNumber).click();
+	ChangeReusables.moveToApprovalState(driver);
+	if( driver.findElement(By.xpath("//button[text()='Follow Up']")).isDisplayed()&&
+			driver.findElement(By.xpath("//button[text()='Update CI']")).isDisplayed()){
+		driver.close();
+		Assert.assertTrue(true);
+		logger.log(Status.PASS, MarkupHelper.createLabel("Test Case passed", ExtentColor.GREEN));
+		}else{
+			System.out.println("invisible");
+		
+	
+		}
+	
+	
+}
+@Test(priority=2,description="Approval of chnage ticket and Moving the Change to Multiple phase",enabled=true)
+public void testUpdateCIErrorMessage() throws Exception{
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	
+    driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ServiceNowUtils.navigateToModuleName2(driver, "change");
+	crNumber = ChangeReusables.createEmerChange(driver,1,2);
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ChangeReusables.verifyStateOfChangeTicket(driver, "Draft", crNumber,1,2);
+	ChangePage.getSubmitForPlanningBtn(driver).click();
+	Thread.sleep(10000);
+    ChangeReusables.moveToAssessmentState(driver);
+	ChangeReusables.FinalReport(driver, "Assessment", crNumber, 2, 2);
+    ChangeReusables.schedule(driver);
+	ChangePage.getUpdateBtn(driver).click();
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+	ChangePage.getChangeNumberFromQueue(driver, crNumber).click();
+	ChangeReusables.moveToApprovalState(driver);
+	/*if( driver.findElement(By.xpath("//button[text()='Follow Up']")).isDisplayed()&&
+			driver.findElement(By.xpath("//button[text()='Update CI']")).isDisplayed()){
+		driver.close();
+		Assert.assertTrue(true);
+		logger.log(Status.PASS, MarkupHelper.createLabel("Test Case passed", ExtentColor.GREEN));
+		}else{
+			System.out.println("invisible");
+		
+	
+		}*/	
+	ChangePage.getUpdateCIBtn(driver).click();
+	String e=driver.switchTo().alert().getText();
+	System.out.println(e);
+	if(e.equalsIgnoreCase("Updates to the CI's will cause approvals to be reset.  Select OK to Continue or Cancel to remain in the current Change State."));
+	{
+		driver.close();
+		Assert.assertTrue(true);
+		logger.log(Status.PASS, MarkupHelper.createLabel("Test Case passed", ExtentColor.GREEN));
+	}
+
+	
+}
+
+@Test(priority=2,description="Approval of chnage ticket and Moving the Change to Multiple phase",enabled=true)
+public void testFollowUpListView() throws Exception{
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	
+    driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ServiceNowUtils.navigateToModuleName2(driver, "change");
+	crNumber = ChangeReusables.createEmerChange(driver,1,2);
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ChangeReusables.verifyStateOfChangeTicket(driver, "Draft", crNumber,1,2);
+	ChangePage.getSubmitForPlanningBtn(driver).click();
+	Thread.sleep(10000);
+    ChangeReusables.moveToAssessmentState(driver);
+	ChangeReusables.FinalReport(driver, "Assessment", crNumber, 2, 2);
+    ChangeReusables.schedule(driver);
+	ChangePage.getUpdateBtn(driver).click();
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+	ChangePage.getChangeNumberFromQueue(driver, crNumber).click();
+	ChangeReusables.moveToApprovalState(driver);
+	ChangePage.getFollowUpBtn(driver).click();
+    Thread.sleep(5000);
+	String actualTitle = driver.getTitle();
+	System.out.println(actualTitle);
+	String expectedTitle = "Change Requests | ServiceNow";
+	assertEquals(expectedTitle,actualTitle);
+	Assert.assertTrue(true);
+	logger.log(Status.PASS, MarkupHelper.createLabel("Test Case passed", ExtentColor.GREEN));
+	
+}
+@Test(priority=0,description="Creation of a CR Ticket",enabled=true)
+public void testFollowUpPlanningState() throws IOException, InterruptedException{
+	logger = extent.createTest("Creation of a CR Ticket");
+	String FirefoxDriver = Capabilities.getPropertyValue("FirefoxDriver");
+	System.setProperty("webdriver.gecko.driver",FirefoxDriver);
+	WebDriver driver=new FirefoxDriver();
+	driver.get("http://thomsonreutersqa.service-now.com");
+    SafeLogin.logInUser(driver);
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ServiceNowUtils.navigateToModuleName2(driver, "change");
+	crNumber = ChangeReusables.createChange(driver,1,2);
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();	
+	WaitUtils.waitForPageToLoad(driver, 10);
+	ChangePage.getSubmitForPlanningBtn(driver).click();
+	Thread.sleep(10000);
+    ChangeReusables.moveToAssessmentState(driver);
+	ChangeReusables.FinalReport(driver, "Assessment", crNumber, 2, 2);
+    ChangeReusables.schedule(driver);
+	ChangePage.getUpdateBtn(driver).click();
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+	ChangePage.getChangeNumberFromQueue(driver, crNumber).click();
+	ChangeReusables.moveToApprovalState(driver);
+	ChangePage.getFollowUpBtn(driver).click();
+	ChangeReusables.searchDesiredChangeTicket(driver, crNumber);
+    ChangePage.getChangeNumberFromQueue(driver, crNumber).click();
+    //String state=ChangePage.getStateField(driver).getAttribute("value");
+    System.out.println(ChangePage.getStateField(driver).getText());
+	//ChangeReusables.verifyStateOfChangeTicket(driver, "Draft", crNumber,1,2);
+	//driver.close();
+	//Assert.assertTrue(true);
+	//logger.log(Status.PASS, MarkupHelper.createLabel("Test Case Passed is Creation of a CR Ticket", ExtentColor.GREEN));
+}
 
 
 }
